09189cca75346f69b0d5293f428ad9f08ed8cd9a
Jacek Wielemborek <d33tah@gmail.com>
2022-02-08 19:58:55 +0100

    add title

--- main.py
+++ main.py
@@ -119,6 +119,7 @@ def podglad():
     else:
         return """
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>labelprinter</title>
         <body onload="przerysujObrazek()">
         <script>
         function przerysujObrazek() {
